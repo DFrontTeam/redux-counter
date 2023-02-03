852ea787ba64d7d9e10991e0b5d0efb000d62065
DFrontTeam <122452114+DFrontTeam@users.noreply.github.com>
2023-02-03 14:40:36 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createStore } from 'redux'
 
 import App from './App';
 
+const store = createStore(rootReducer)
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-
 root.render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>,
 );
